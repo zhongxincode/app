8d2dfee4bc2fda935028ffca1fd99c50f43e2153
zx <3313147896@qq.com>
2022-04-25 22:40:51 +0800

    first commit

---
+++ src/api/index.js
@@ -0,0 +1,45 @@
+import ajax from "@/common/ajax";
+import mock from "../mock/index";
+const request = process.env.REACT_APP_MOCK === "1" ? mock : ajax;
+const getMenu = () => request.get("/getmenu");
+const getMenuList = () => request.get("/getmenulist");
+const login = (data) => request.post("/login", data);
+const addMenu = (data) => request.post("/addmenu", data);
+const addMsg = (data) => request.post("/addmessage", data);
+const getMsg = (data) => request.post("/getmessage", data);
+const getPower = () => request.get("/getpower");
+const delMenu = (data) => request.post("/delmenu", data);
+const getMenuInfo = (data) => request.post("/getmenuinfo", data);
+const editMenu = (data) => request.post("/editmenuinfo", data);
+const getVisitorList = (data) => request.post("/getiplist", data);
+const getVisitorData = () => request.get("/getvisitordata");
+const getUserList = (data) => request.post("/getuserlist", data);
+const addUser = (data) => request.post("/adduserinfo", data);
+const getUser = (data) => request.post("/getuserinfo", data);
+const editUser = (data) => request.post("/edituserinfo", data);
+const editType = (data) => request.post("/edittype", data);
+const addType = (data) => request.post("/addtype", data);
+const getFeedBack = (data) => request.post("/getfeedback", data);
+const reply = (data) => request.post("/reply", data);
+export {
+  getMenu,
+  login,
+  addMenu,
+  addMsg,
+  getMsg,
+  getPower,
+  delMenu,
+  getMenuInfo,
+  editMenu,
+  getVisitorList,
+  getVisitorData,
+  getUserList,
+  addUser,
+  getUser,
+  editUser,
+  editType,
+  addType,
+  getMenuList,
+  getFeedBack,
+  reply,
+};
---
+++ src/store/layout/action.js
@@ -0,0 +1,5 @@
+
+export const changeLayoutMode = (mode) => ({
+  type: mode,
+  mode,
+});
